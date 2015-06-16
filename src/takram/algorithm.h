a3159aa7530177b47772e67f9bc85e10b893c56b
Shota Matsuda <s@sgss.co>
2015-06-17 01:26:53 +0900

    header

--- src/takram/algorithm.h
+++ src/takram/algorithm.h
@@ -28,7 +28,7 @@
 #ifndef TAKRAM_ALGORITHM_H_
 #define TAKRAM_ALGORITHM_H_
 
-#include "takram/algorithm/tuple_iterator.h"
+#include "takram/algorithm/leaf_iterator_iterator.h"
 #include "takram/algorithm/variadic_template.h"
 
 #endif  // TAKRAM_ALGORITHM_H_
